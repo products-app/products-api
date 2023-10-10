ccd01e894d4b9e2e1ec1f6fad0d94bb08c4193d8
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-10 13:58:30 -0300

    feat: add fastify cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
-import fastify from "fastify";
+import Fastify from "fastify";
 
-export const app = fastify();
+const app = Fastify();
+
+app.register(require('@fastify/cors'), () => {
+  return (req: { headers: { origin: string; }; }, callback: (arg0: null, arg1: {
+      origin: boolean;
+    }) => void) => {
+    const corsOptions = {
+      origin: true
+    };
+
+    if (/^localhost$/m.test(req.headers.origin)) {
+      corsOptions.origin = false
+    }
+
+    callback(null, corsOptions)
+  }
+})
+
+export { app };
