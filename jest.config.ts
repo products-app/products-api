e6c440ba378183cd5c932be35a98396825634b00
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-23 15:28:42 -0300

    feat: add test configs for circle ci
    Add some jobs in circle ci: build app; test with jest; generate code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,10 @@ import type { Config } from 'jest'
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: 'coverage',
+  coverageDirectory: './coverage/',
   coverageProvider: 'v8',
   preset: 'ts-jest',
-  setupFiles: ['dotenv/config'],
+  setupFiles: ['dotenv/config', '<rootDir>/tests/setEnvVars.ts'],
   setupFilesAfterEnv: [
     '<rootDir>/tests/__mocks__/prisma.ts',
     '<rootDir>/tests/__mocks__/stripe.ts',
---
+++ tests/setEnvVars.ts
@@ -0,0 +1,1 @@
+process.env.PRIVATE_KEY = 'jwt_private_key'
